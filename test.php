e0907113cc29f931a4827b33d81af09a9461be8b
zhaoming <kd_gzm@sina.cn>
2021-06-16 12:11:26 +0800

    test

--- test.php
+++ test.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $obj =  new \Zhaoming\tools\ToolsService();
 
+//test
 #解压文件 zip,rar,7z
 # MAC系统需要支持 安装 zip,rar,7z 解包工具
 //$obj->tools('unzip')->unPackage('/dir/');
